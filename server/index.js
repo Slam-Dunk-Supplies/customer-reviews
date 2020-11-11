409e4807286d2224bbe14d9a62e8f5e9fe09ca82
Ying Guo <guoguoying2013@gmail.com>
2020-11-11 11:42:37 -0800

    webpack is done and react setup is done

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ const path = require('path');
 const morgan = require('morgan');
 
 app.use(morgan('dev'));
-
+app.use(express.static(path.resolve(__dirname, '../client/dist')));
 
 app.listen(PORT, () => {
     console.log('app is running on localhost:3000');
