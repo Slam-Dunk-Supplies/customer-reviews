d6cad730345891be6cdc88f4cf101a6cedac1596
Ying Guo <guoguoying2013@gmail.com>
2020-11-11 09:41:12 -0800

    npm initiated and server is running

--- server/index.js
+++ server/index.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const app = express();
+
+const PORT = 3000;
+const path = require('path');
+const morgan = require('morgan');
+
+app.use(morgan('dev'));
+
+
+app.listen(PORT, () => {
+    console.log('app is running on localhost:3000');
+})
