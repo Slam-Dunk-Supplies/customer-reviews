71cad3145ae8b6af17df20058c50d336758cdd4e
Ying Guo <guoguoying2013@gmail.com>
2020-11-13 09:43:09 -0800

    used hasMany method in sequelize and tested by adding new endpoint in server to retrieve data with association relationship

--- database/index.js
+++ database/index.js
@@ -52,9 +52,13 @@ var Customers = orm.define('customers', {
   }
 });
 
+Customers.hasMany(Reviews, {
+  foreignKey: {
+    name: 'customer_id',
+    allowNull: false
+  }
+});
 // Reviews.belongsTo(Customers, {foreignKey: 'customer_id'});
-// Customers.hasMany(Reviews)
-// Reviews.belongsTo(Products, {foreignKey: 'product_id'});
 
 Products.sync();
 Customers.sync();
