e4b4c417356f59d33ace411a41f6d7a38547b295
Ying Guo <guoguoying2013@gmail.com>
2020-11-15 11:53:48 -0800

    added all files for commit

--- webpack.config.js
+++ webpack.config.js
@@ -13,13 +13,13 @@ module.exports = {
         test: /\.jsx$/,
         exclude: /node_modules/,
         use: {
-          loader: "babel-loader",
+          loader: 'babel-loader',
           options: {
-            presets: ['@babel/preset-env', '@babel/preset-react']
-          }
-        }
-      }
-    ]
+            presets: ['@babel/preset-env', '@babel/preset-react'],
+          },
+        },
+      },
+    ],
   },
-  mode: 'development'
+  mode: 'development',
 };
